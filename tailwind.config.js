fd8d04550f6a2e0cafdb844b32e85220c57e1972
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-08-04 17:44:53 +0530

    tailwind introduced

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx,md}',
+    './components/**/*.{js,ts,jsx,tsx,mdx,md}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
